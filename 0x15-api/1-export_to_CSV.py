5cc9bd03de2c6ada7bdd3005a3c8cf37f77d9995
SeckMohameth <mohamethseck34@gmail.com>
2019-09-16 19:50:14 +0000

    not naming file with .csv extension

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Script for restful api"""
+import requests
+import sys
+import csv
+
+if __name__ == "__main__":
+
+    """Get info about user"""
+
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    filename = user_id = sys.argv[1]
+
+    employee = requests.get(url + "users/" + user_id)
+    employee = employee.json()
+
+    tasks = requests.get(url + "todos?userId=" + user_id)
+    tasks = tasks.json()
+
+    with open(filename, mode="w") as csv_file:
+        writer = csv.writer(csv_file, delimiter=',',
+                            quotechar='"', quoting=csv.QUOTE_ALL)
+        for item in tasks:
+            writer.writerow((item.get('userId'), employee.get('username'),
+                            item.get('completed'), item.get('title')))
