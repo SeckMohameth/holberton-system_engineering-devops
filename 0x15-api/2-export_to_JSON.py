621174173ef47cbcdb0fbf4d81f135240928c6f0
SeckMohameth <mohamethseck34@gmail.com>
2019-09-16 20:28:16 +0000

    export to json

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Script for restful api"""
+import requests
+import sys
+import json
+
+if __name__ == "__main__":
+
+    """Get info about user"""
+
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    filename = user_id + ".json"
+
+    employee = requests.get(url + "users/" + user_id)
+    employee = employee.json()
+
+    tasks = requests.get(url + "todos?userId=" + user_id)
+    tasks = tasks.json()
+
+
+    task_list = []
+    for x in tasks:
+        task_list.append(x)
+
+    stuff = {}
+    stuff[user_id] = task_list
+
+    with open(filename, mode="w") as json_file:
+        json.dump(stuff, json_file)
