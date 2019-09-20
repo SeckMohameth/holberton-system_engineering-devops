7f11cfc6cd7b63a729ae6b60dc652e6d3059e257
SeckMohameth <mohamethseck34@gmail.com>
2019-09-20 02:01:04 +0000

    main not working

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+
+def number_of_subscribers(subreddit):
+    import requests
+    url = 'https://www.reddit.com/r/{}/new.json?sort=new'.format(subreddit)
+
+    headers = {
+        'User-Agent': 'My User Agent 1.0'
+    }
+
+    reditt = request.get(url, headers=headers)
+
+    if r.status_code != 200:
+        print(None)
+        return
+
+    try:
+        json = reditt.json()
+        x = json.get('data').get('children')
+        for i in x:
+            print(i.get('data').get('title'))
+    except Exception:
+        pass
